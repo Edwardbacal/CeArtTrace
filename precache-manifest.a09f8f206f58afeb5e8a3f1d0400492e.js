dc62756141b5ec50fc30b05997673c19b89a305e
Edward Bacal <edwarddbacal@gmail.com>
2020-07-17 09:25:52 -0400

    Updates

---
+++ precache-manifest.a09f8f206f58afeb5e8a3f1d0400492e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd981c9b5324867ab66ab91c8ebc17a",
+    "url": "/CeArtTrace/index.html"
+  },
+  {
+    "revision": "e2611855a1fe8e4d7efa",
+    "url": "/CeArtTrace/static/css/main.f8b6719b.chunk.css"
+  },
+  {
+    "revision": "bcc4405f7b489aee2bb7",
+    "url": "/CeArtTrace/static/js/2.832f8cc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CeArtTrace/static/js/2.832f8cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2611855a1fe8e4d7efa",
+    "url": "/CeArtTrace/static/js/main.6743a910.chunk.js"
+  },
+  {
+    "revision": "59c4774662c05515213c",
+    "url": "/CeArtTrace/static/js/runtime-main.42675915.js"
+  },
+  {
+    "revision": "de9b2e40f269ff48ca823d9dbe2e551b",
+    "url": "/CeArtTrace/static/media/harv.de9b2e40.png"
+  },
+  {
+    "revision": "95453fc3965dcc78576e609a2c58df82",
+    "url": "/CeArtTrace/static/media/paper.95453fc3.jpeg"
+  }
+]);
