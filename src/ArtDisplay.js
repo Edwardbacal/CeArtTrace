9cf3ce56874b4ccfcfda21c9c0eb855feef1ca60
Edward Bacal <edwarddbacal@gmail.com>
2020-08-01 19:20:51 -0400

    layout fixes

--- src/ArtDisplay.js
+++ src/ArtDisplay.js
@@ -3,17 +3,19 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const ArtDisplay = ({artInfo: {image, name, title, date}}) => {
   return (
-    <div className="ArtDisplay clearfix" id="display">
-      <div className="imageCont">
-        <img src={image} alt="" />
+    <section id="display" className="ArtDisplay">
+      <div className="ArtDisplayTwo">
+        <div className="imageCont">
+          <img src={image} alt="" />
+        </div>
+        <div className="displayText">     
+          <h3>{name}</h3>
+          <h3>{title} </h3>
+          <h3>{date}</h3>
+          <AnchorLink href='#start'><button>Again!</button></AnchorLink>
+        </div>
       </div>
-      <div className="displayText">     
-        <h3>{name}</h3>
-        <h3>{title} </h3>
-        <h3>{date}</h3>
-        <AnchorLink href='#start'><button>Again!</button></AnchorLink>
-      </div>
-    </div>
+    </section>
   )
 }
 
